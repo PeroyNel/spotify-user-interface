075c2c8147bc9ae0abc68e82c7996fee0a690e62
Peroy Nel <peroynel@yahoo.com>
2022-09-27 10:13:44 +0200

    Initial Commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
+import heavyRotation from '../../assets/mockdata/heavyRotation.json';
+import jumpBackIn from '../../assets/mockdata/jumpBackIn.json';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
