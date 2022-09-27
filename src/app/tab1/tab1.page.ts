3ee8af2125fb51d104738235c95dcac477906d73
Peroy Nel <peroynel@yahoo.com>
2022-09-27 16:01:30 +0200

    Main Page Implemented
    Main Page Implemented with Side Scroll Functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,13 +3,43 @@ import { Component } from '@angular/core';
 import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
 import heavyRotation from '../../assets/mockdata/heavyRotation.json';
 import jumpBackIn from '../../assets/mockdata/jumpBackIn.json';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+
+
+  data = [
+    {
+      title: 'Recently Played',
+      albums: recentlyPlayed
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation
+    },
+    {
+      title: 'Jump back in',
+      albums: jumpBackIn
+    }
+  ];
+
+  opts = {
+    slidesPerView: 2.4,
+    slidesOffsetBefore: 20,
+    spaceBetween: 20,
+    freeMode: true,
+  }
 
   constructor() {}
 
+  dasherize(string) {
+    return string.replace(/[A-Z]/g, function(char, index) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  };
 }
